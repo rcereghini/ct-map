2ac7bd64bf5dae25b7e376e52ba7ede382e3dc12
Robert Cereghini <rcereghini@gmail.com>
2020-01-12 20:52:37 -0700

    Old project. Lack of file structure bugged me. No other changes.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,69 @@
-import React from 'react'
-import './App.css'
-import Welcome from './Welcome.js'
-import Clock from './Clock.js'
-import Toggle from './Toggle.js'
-import Greeting from './Greeting.js'
-import Map from './Map.js'
+import React from "react";
+import "./App.css";
+import Clock from "./components/Clock/Clock.js";
+import Map from "./components/Map/Map.js";
 
 const events = [
   {
-    title: 'Lunch and Learn',
-    date: 'June 25th, 2018',
-    time: '3:00PM - 5:00PM'
+    title: "Lunch and Learn",
+    date: "June 25th, 2018",
+    time: "3:00PM - 5:00PM"
   },
   {
-    title: 'Escrow Escape Room',
-    date: 'July 6th, 2018',
-    time: '11:00AM - 1:00PM'
+    title: "Escrow Escape Room",
+    date: "July 6th, 2018",
+    time: "11:00AM - 1:00PM"
   },
   {
-    title: 'Disco Night',
-    date: 'July 14th, 2018',
-    time: '7:00PM - 11:00PM'
+    title: "Disco Night",
+    date: "July 14th, 2018",
+    time: "7:00PM - 11:00PM"
   },
   {
-    title: 'Taco Tuesday',
-    date: 'July 23rd, 2018',
-    time: '12:00PM - 2:00PM'
+    title: "Taco Tuesday",
+    date: "July 23rd, 2018",
+    time: "12:00PM - 2:00PM"
   }
-]
-
+];
 
 const App = () => {
-  return(
-  <div className='fullPage'>
-    <div className='header'>
-      <img alt='' src="http://www.cleartitleaz.com/wp-content/uploads/2016/01/logo.png" style={{height:'65px'}}/>
-    </div>
-    <div className='mapHolder'>
-      <Map />
-      <div className='eventContainer'>
-        <div className='eventBox eventBox-top'><h2>{events[0].title}</h2><h3>{events[0].date}</h3><h3>{events[0].time}</h3></div>
-        <div className='eventBox'><h2>{events[1].title}</h2><h3>{events[1].date}</h3><h3>{events[1].time}</h3></div>
-        <div className='eventBox'><h2>{events[2].title}</h2><h3>{events[2].date}</h3><h3>{events[2].time}</h3></div>
-        <div className='eventBox eventBox-bottom'><h2>{events[3].title}</h2><h3>{events[3].date}</h3><h3>{events[3].time}</h3></div>
+  return (
+    <div className="fullPage">
+      <div className="header">
+        <img
+          alt=""
+          src="http://www.cleartitleaz.com/wp-content/uploads/2016/01/logo.png"
+          style={{ height: "65px" }}
+        />
       </div>
+      <div className="mapHolder">
+        <Map />
+        <div className="eventContainer">
+          <div className="eventBox eventBox-top">
+            <h2>{events[0].title}</h2>
+            <h3>{events[0].date}</h3>
+            <h3>{events[0].time}</h3>
+          </div>
+          <div className="eventBox">
+            <h2>{events[1].title}</h2>
+            <h3>{events[1].date}</h3>
+            <h3>{events[1].time}</h3>
+          </div>
+          <div className="eventBox">
+            <h2>{events[2].title}</h2>
+            <h3>{events[2].date}</h3>
+            <h3>{events[2].time}</h3>
+          </div>
+          <div className="eventBox eventBox-bottom">
+            <h2>{events[3].title}</h2>
+            <h3>{events[3].date}</h3>
+            <h3>{events[3].time}</h3>
+          </div>
+        </div>
+      </div>
+      <Clock />
     </div>
-    <Clock/>
-  </div>
-)}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Map/Map.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import "./Map.css";
+
+const justClosed = [
+  {
+    lat: 33.5154665,
+    lng: -112.0665024
+  },
+  {
+    lat: 33.5319053,
+    lng: -111.9270854
+  },
+  {
+    lat: 33.5141599,
+    lng: -112.0381503
+  },
+  {
+    lat: 33.4820945,
+    lng: -112.0827823
+  },
+  {
+    lat: 33.4397042,
+    lng: -112.0951419
+  },
+  {
+    lat: 33.4222265,
+    lng: -112.0505099
+  }
+];
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default class Map extends Component {
+  static defaultProps = {
+    center: { lat: 33.5154665, lng: -112.0665024 },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <div className="google-map" style={{ height: "100%", width: "70%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <div className="whiteBox">Recently Closed Deals</div>
+          <AnyReactComponent
+            lat={justClosed[0].lat}
+            lng={justClosed[0].lng}
+            text={
+              <img
+                alt="marker"
+                width="30px"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png"
+              />
+            }
+          />
+          <AnyReactComponent
+            lat={justClosed[1].lat}
+            lng={justClosed[1].lng}
+            text={
+              <img
+                alt="marker"
+                width="30px"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png"
+              />
+            }
+          />
+          <AnyReactComponent
+            lat={justClosed[2].lat}
+            lng={justClosed[2].lng}
+            text={
+              <img
+                alt="marker"
+                width="30px"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png"
+              />
+            }
+          />
+          <AnyReactComponent
+            lat={justClosed[3].lat}
+            lng={justClosed[3].lng}
+            text={
+              <img
+                alt="marker"
+                width="30px"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png"
+              />
+            }
+          />
+          <AnyReactComponent
+            lat={justClosed[4].lat}
+            lng={justClosed[4].lng}
+            text={
+              <img
+                alt="marker"
+                width="30px"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png"
+              />
+            }
+          />
+          <AnyReactComponent
+            lat={justClosed[5].lat}
+            lng={justClosed[5].lng}
+            text={
+              <img
+                alt="marker"
+                width="30px"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png"
+              />
+            }
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
